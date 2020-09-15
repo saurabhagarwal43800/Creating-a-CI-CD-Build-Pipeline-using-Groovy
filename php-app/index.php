883e9e3ddb7514f0a5c49566cc8387998fe0c700
Saurabh Agarwal <33370952+saurabhagarwal43800@users.noreply.github.com>
2020-09-15 14:06:38 +0530

    PHP workspace

---
+++ php-app/index.php
@@ -0,0 +1,12 @@
+<html>
+<body bgcolor="#ff8269">
+  <h1>Welcome to Task3</h1>
+<pre>
+<?php
+$color= "red";
+echo "My car is of ".$color." color<br/>";
+print `ifconfig`;
+?>
+</pre>
+</body>
+</html>
